ad22c555a17ce38bfc84278f851447eff7698a64
Igor <igorcossta21@gmail.com>
2022-01-31 12:41:06 -0300

    add config to load svg files

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 const Header: React.FC = () => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
-        Logo
+        <Logo />
       </Link>
       <div className="options">
         <Link className="option" to="/shop">
