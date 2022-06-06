a1f822d7bff15ca08ed3f8fbbb789bb47a7fee13
igorcossta <igorcossta21@gmail.com>
2022-06-06 17:29:04 -0300

    added react lazy optimizations

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import App from '../App';
-import CategoryContainer from '../components/category-container';
-import CheckoutPage from '../pages/checkout';
-import HomePage from '../pages/homepage';
-import NotFound from '../pages/notfound';
-import ShopPage from '../pages/shop';
-import SignInSignUp from '../pages/sign-in-sign-up';
-import CategorySingle from '../components/category-single';
+const App = lazy(() => import('../App'));
+const HomePage = lazy(() => import('../pages/homepage'));
+const CategoryContainer = lazy(
+  () => import('../components/category-container')
+);
+const CheckoutPage = lazy(() => import('../pages/checkout'));
+const SignInSignUp = lazy(() => import('../pages/sign-in-sign-up'));
+const CategorySingle = lazy(() => import('../components/category-single'));
+const NotFound = lazy(() => import('../pages/notfound'));
+const ShopPage = lazy(() => import('../pages/shop'));
 
 const AppRoutes: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
+    <Suspense fallback={<h2>...</h2>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
 
-          <Route path="shop" element={<ShopPage />}>
-            <Route index element={<CategoryContainer />} />
-            <Route path=":categoryId" element={<CategorySingle />} />
+            <Route path="shop" element={<ShopPage />}>
+              <Route index element={<CategoryContainer />} />
+              <Route path=":categoryId" element={<CategorySingle />} />
+            </Route>
+
+            <Route path="signin" element={<SignInSignUp />} />
+            <Route path="checkout" element={<CheckoutPage />} />
+
+            <Route path="*" element={<NotFound />} />
           </Route>
-
-          <Route path="signin" element={<SignInSignUp />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
